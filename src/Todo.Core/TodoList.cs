75559f1692484e51abdaeb62c9afdd49ce669901
maxouni <max1205325@gmail.com>
2025-11-30 20:34:58 +0700

    Add project files.

---
+++ src/Todo.Core/TodoList.cs
@@ -0,0 +1,40 @@
+﻿// -------------------------------------------------------------------
+// <copyright file="TodoList.cs" company="NATK">
+// Copyright (c) NATK. All rights reverved.
+// </copyright>
+// -------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Core
+{
+    public class TodoList
+    {
+        private readonly List<TodoItem> items = new(); // dwdaqdwa
+        public IReadOnlyList<TodoItem> Items => items.AsReadOnly();
+
+        public TodoItem Add(string title)
+        {
+            TodoItem item = new(title);
+            this.items.Add(item);
+            return item;
+        }
+
+        public bool Remove(Guid id)
+        {
+            return this.items.RemoveAll(i => i.Id == id) > 0;
+        }
+
+        public IEnumerable<TodoItem> Find(string substring)
+        {
+            return this.items.Where(i =>
+                i.Title.Contains(substring ?? string.Empty, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public int Count => this.items.Count;
+    }
+}
